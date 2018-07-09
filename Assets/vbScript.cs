f387e507e9c4c91433e469ad065bc1c49af7b5a9
Raj <raj.kadhi10@gmail.com>
2018-07-09 16:25:17 +0530

    Hello

---
+++ Assets/vbScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour,IVirtualButtonEventHandler
+{
+
+	private GameObject vbbuttonobject;
+	private GameObject zombie;
+	// Use this for initialization
+	void Start ()
+	{
+		vbbuttonobject = GameObject.Find ("actionbutton");
+		zombie = GameObject.Find ("zombie");
+		vbbuttonobject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+	{
+		Debug.Log ("Button yeahhh");
+		zombie.GetComponent<Animation> ().Play ();
+	}
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+	{
+		zombie.GetComponent<Animation> ().Stop ();
+	}
+
+}
